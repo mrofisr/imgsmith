b9169e93801b0484dcf4d31bfdab0207ce74db93
abdurrofi <rofim58@gmail.com>
2026-01-06 18:56:57 +0700

    feat: export recommendation engine from main index
    - Export analyzeImage, recommendFormat, rules, and guidelines
    - Available as part of core API
    - Complete recommendation system accessible to users

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@
 export * from "./core/types.js";
 export * from "./core/converter.js";
 
+// Recommendation engine
+export * from "./recommendation/engine.js";
+
 // Individual converters (advanced usage)
 export * from "./converters/jpeg.js";
 export * from "./converters/png.js";
